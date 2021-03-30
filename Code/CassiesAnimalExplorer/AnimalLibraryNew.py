ad83a7a2ff8ab9243e80029e9f37f31e77618dac
Evan Lazzaro <lazzaroevan@gmail.com>
2021-03-30 15:02:29 -0400

    Lots of work done on project

---
+++ Code/CassiesAnimalExplorer/AnimalLibraryNew.py
@@ -0,0 +1,138 @@
+import os
+import tkinter as tk
+from PIL import ImageTk, Image
+import tkinter.font as font
+import random
+
+window = tk.Tk()
+#class
+class VerticalScrolledFrame(tk.Frame):
+    def __init__(self, parent, *args, **kw):
+        tk.Frame.__init__(self, parent, *args, **kw)
+
+        # create a canvas object and a vertical scrollbar for scrolling it
+        vscrollbar = tk.Scrollbar(self, orient=tk.VERTICAL,width = 50)
+        vscrollbar.pack(fill=tk.Y, side=tk.RIGHT, expand=tk.FALSE)
+        canvas = tk.Canvas(self, bd=0, highlightthickness=0,yscrollcommand=vscrollbar.set,height = (window.winfo_screenheight()-50))
+        canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=tk.TRUE)
+        vscrollbar.config(command=canvas.yview)
+
+        # reset the view
+        canvas.xview_moveto(0)
+        canvas.yview_moveto(0)
+
+        # create a frame inside the canvas which will be scrolled with it
+        self.interior = interior = tk.Frame(canvas)
+        interior_id = canvas.create_window(0, 0, window=interior,anchor=tk.NW)
+
+        # track changes to the canvas and frame width and sync them,
+        # also updating the scrollbar
+        def _configure_interior(event):
+            # update the scrollbars to match the size of the inner frame
+            size = (interior.winfo_reqwidth(), interior.winfo_reqheight())
+            canvas.config(scrollregion="0 0 %s %s" % size)
+            if interior.winfo_reqwidth() != canvas.winfo_width():
+                # update the canvas's width to fit the inner frame
+                canvas.config(width=interior.winfo_reqwidth())
+
+        interior.bind('<Configure>', _configure_interior)
+        def _configure_canvas(event):
+            if interior.winfo_reqwidth() != canvas.winfo_width():
+                # update the inner frame's width to fill the canvas
+                canvas.itemconfigure(interior_id, width=canvas.winfo_width())
+        canvas.bind('<Configure>', _configure_canvas)
+        # This is what enables scrolling with the mouse:
+
+#variables
+#importing list of animals for buttons
+def importAnimalNames(filepath):
+    files = os.walk(filepath)
+    animalNames = []
+    for i in files:
+        animalNames.append(i[2])
+    return animalNames[0]
+
+def upOrDownPressedFunc(int,upOrDownPressed,animals,animalNames):
+    if((int == -1) and upOrDownPressed[0]>0):
+        upOrDownPressed[0] += int
+        print(upOrDownPressed[0])
+    elif(int == 1):
+        upOrDownPressed[0] += int
+        print(upOrDownPressed[0])
+    for i in range(len(animals)):
+        animals[i]['text'] = animalNames[i+upOrDownPressed[0]]
+
+def infoShower(frame,title,frameToDestroy,window,font):
+    print(title)
+    file = open('Summaries/'+title,'r',encoding='utf8')
+    string = title + '\n'
+    for i in file:
+        string += i
+    fontToUse = font
+    frame.destroy()
+    frameToDestroy.destroy()
+    newFrame = VerticalScrolledFrame(window)
+    newFrame.pack()
+    label = tk.Label(newFrame.interior, text=string,font=fontToUse, wraplength=(window.winfo_screenwidth() - 50))
+    label.pack(side='top', fill='both', expand='yes')
+    bottomBar = tk.Frame(window)
+    button = tk.Button(bottomBar, font=fontToUse, text='BACK', height=30, border=10,
+                       command=lambda: restart(newFrame, bottomBar))
+    button.pack(side='bottom')
+    for i in os.walk("Images/"+title):
+        for a in i[2]:
+            img = ImageTk.PhotoImage(Image.open('Images/'+title+'/'+a))
+            imageLabel = tk.Label(master = newFrame.interior, image=img)
+            imageLabel.img = img
+            imageLabel.pack(side='bottom', fill='both', expand='yes')
+    bottomBar.pack()
+
+def restart(frame,bottombar):
+    frame.destroy()
+    bottombar.destroy()
+    main()
+
+def randomAnimal(frame,animalNames,bottomBar):
+    print('random')
+
+#importing photo database
+
+
+#generating window
+
+
+#frame
+
+def main():
+    window.minsize(window.winfo_screenwidth(), window.winfo_screenheight())
+    window.maxsize(window.winfo_screenwidth(), window.winfo_screenheight())
+    window.overrideredirect(1)
+    window.resizable(0, 0)
+    window.title("Animal Encyclopedia")
+    animalNames = importAnimalNames('Summaries')
+    fontToUse = font.Font(family='Times New Roman', size=16, weight='bold')
+    upOrDownPressed = [0]
+    animalButtons = []
+    frame = tk.Frame()
+    bottomBar = tk.Frame()
+    randomButton = tk.Button(bottomBar, text='RANDOM', height=1, border=10, font=fontToUse,
+                             command=lambda: randomAnimal(frame, animalNames, bottomBar))
+    downButton = tk.Button(bottomBar, text='DOWN', height=1, border=10, font=fontToUse,
+                           command=lambda: upOrDownPressedFunc(1, upOrDownPressed, animalButtons,animalNames))
+    upButton = tk.Button(bottomBar, text='UP', height=1, border=10, font=fontToUse,
+                         command=lambda: upOrDownPressedFunc(-1, upOrDownPressed, animalButtons,animalNames))
+    for i in range(21):
+        newButton = (tk.Button(master = frame,font=fontToUse, border='5', text=animalNames[i], width=(window.winfo_screenwidth()),
+                               bg='lightblue', fg='black',
+                               command=lambda c=i: infoShower(frame, animalButtons[c].cget("text"), bottomBar,window,fontToUse)))
+        animalButtons.append(newButton)
+        animalButtons[i].pack()
+    upButton.pack(side=tk.LEFT)
+    randomButton.pack(side=tk.LEFT)
+    downButton.pack(side=tk.RIGHT)
+    frame.pack()
+    bottomBar.pack()
+    window.mainloop()
+
+if __name__ == '__main__':
+    main()
--- Code/CassiesAnimalExplorer/Cassie's Library Maker.py
+++ Code/CassiesAnimalExplorer/Cassie's Library Maker.py
@@ -1,7 +1,11 @@
+import shutil
+
+import requests
 from mediawiki import MediaWiki
 from mediawiki import DisambiguationError
 import threading
 import pickle
+import os
 wikipedia = MediaWiki()
 
 def getListOfAnimals(title,path,delimiter):
@@ -28,7 +32,7 @@ def saveDictToFile(dict,filepath):
     file = open(filepath, "wb")
     pickle.dump(dict, file)
     file.close()
-def getData(checkForReptile,animalDict,breakout,threadIncrement):
+def getData(checkForReptile,animalDict,breakout,threadIncrement,errors):
     finalSize = len(checkForReptile)
     currentSize = 0
     for i in checkForReptile[breakout:breakout+threadIncrement]:
@@ -41,28 +45,42 @@ def getData(checkForReptile,animalDict,breakout,threadIncrement):
                 else:
                     firstWord = tempTitle
                 if firstWord not in animalDict:
-                    animalDict[firstWord] = [(tempTitle,childPage.summary,childPage.images)]
+                    animalDict[firstWord] = [tempTitle]
                 else:
-                    animalDict[firstWord].append((tempTitle,childPage.summary,childPage.images))
-        except(DisambiguationError):
-            print('Disambiguation Error')
-
+                    animalDict[firstWord].append(tempTitle)
+                os.mkdir(os.getcwd()+'/Images/' + tempTitle)
+                file = open('Summaries/'+ tempTitle,'w',encoding='utf8')
+                file.write(childPage.summary)
+                file.close()
+                for i in childPage.images:
+                    filename = i.split("/")[-1]
+                    r = requests.get(i, stream=True)
+                    if r.status_code == 200:
+                        r.raw.decode_content = True
+                        with open(os.getcwd()+'/Images/' + tempTitle+ '/' + filename, 'wb') as f:
+                            shutil.copyfileobj(r.raw, f)
+                    else:
+                       errors[0] += 1
+        except(DisambiguationError,FileExistsError):
+            errors[0] += 1
         if threadIncrement == 0:
             break
         else:
             threadIncrement -= 1
         currentSize += 1
         if(currentSize%100 == 0):
-            print(currentSize,'/',threadIncrement,sep='')
+            print(currentSize,'/',threadIncrement,sep='',end='')
+            print('Errors:',errors[0])
 
 if __name__ == '__main__':
     #getListOfAnimals('Animals','animalDatabase.txt','#')
     animalDatabase = importFile('animalDatabase.txt','#')
     arrayOfThreads =[]
     animalDict = {}
-    threadIncrement = len(animalDatabase)//10
-    for i in range(10):
-        thread = threading.Thread(target=getData, args=(animalDatabase,animalDict,i*threadIncrement,threadIncrement))
+    threadIncrement = len(animalDatabase)//13
+    errors = [0]
+    for i in range(13):
+        thread = threading.Thread(target=getData, args=(animalDatabase,animalDict,i*threadIncrement,threadIncrement,errors))
         arrayOfThreads.append(thread)
         thread.start()
     for i in arrayOfThreads:
